ecd57837752611fa77996b268ed39dc381448806
Devbot0716 <pzepher@gmail.com>
2018-06-23 13:07:38 -0600

    done all but black diamond

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class Home extends Component {
+export default class ClassList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      students: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${ this.props.match.params.class }`).then( results => {
+      this.setState({
+        students: results.data
+      });
+    });
+  }
+
   render() {
+    const students = this.state.students.map((student, i) => (
+      <h3 key={ i }>{ student.first_name } { student.last_name }</h3>
+    ));
+
     return (
-      <div className="box">
-        <button className='btn'>Math 1010</button>
-        <button className='btn'>English 2010</button>
-        <button className='btn'>Biology 2020</button>
+      <div className='box'>
+        <h1>{ this.props.match.params.class }</h1>
+        <h2>ClassList:</h2>
+        { students }
       </div>
-    );
+    )
   }
 }
